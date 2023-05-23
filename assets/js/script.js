c11a5799d009e4648c3ca02fcb611698fe9de79d
Caio Matheus dos Santos Abra <caioabra41@gmail.com>
2023-05-23 17:59:26 -0300

    animação de scroll

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    var totalItems = $('ul.side-nav li').length;
+    var currentItem = 0;
+
+    function showItem(index) {
+        $('ul.side-nav li').removeClass('is-active');
+        $('ul.side-nav li').eq(index).addClass('is-active');
+        $('#space li.l-section.section').removeClass('section--is-active');
+        $('#space li.l-section.section').eq(index).addClass('section--is-active');
+    }
+
+    function scrollNext() {
+        currentItem++;
+        if (currentItem >= totalItems) {
+            currentItem = 0;
+        }
+        showItem(currentItem);
+    }
+
+    function scrollPrev() {
+        currentItem--;
+        if (currentItem < 0) {
+            currentItem = totalItems - 1;
+        }
+        showItem(currentItem);
+    }
+
+    $('.container').on('wheel', function (event) {
+        if (event.originalEvent.deltaY > 0) {
+            scrollNext();
+        } else {
+            scrollPrev();
+        }
+    });
+});
--- views/home.php
+++ views/home.php
@@ -8,13 +8,272 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="start-up cooperativa focada em desenvolvimento de apps, bots e games">
     <meta name="keywords" content="apps, desenvolvimento, games, bots, websites, studio">
+    <link rel="icon" type="image/x-icon" href="./assets/img/favicon.png">
+
     <title>CWB TECH STUDIO</title>
+
+    <link rel="stylesheet" href="./assets/css/style.css">
+
 </head>
 
 <body>
-    <h1>
-        Home
-    </h1>
+    <div>
+        <div class="container">
+            <div class="outer-nav--return"></div>
+
+            <div id="viewport" class="l-viewport">
+                <div class="l-wrapper">
+                    <header class="header">
+                        <button class="header--cta cta">CWB TECH STUDIO</button>
+                        <div class="header--nav-toggle">
+                            <span></span>
+                        </div>
+                    </header>
+
+                    <nav class="l-side-nav">
+                        <ul class="side-nav">
+                            <li class="is-active"><span>Home</span></li>
+                            <li class=""><span>Produtos</span></li>
+                            <li class=""><span>Sobre nós</span></li>
+                            <li class=""><span>Contato</span></li>
+                        </ul>
+                    </nav>
+
+                    <ul id="space" class="l-main-content main-content">
+                        <li class="l-section section section--is-active">
+                            <div class="intro" id="home">
+                                <div class="intro--banner">
+                                    <h1>CWB TECH STUDIO<br></h1>
+                                    <button class="cta" style="background-color:#2653c4">ENTRE EM CONTATO
+                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 150 118" style="enable-background:new 0 0 150 118;"xml:space="preserve">
+                                            <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
+                                                <path
+                                                    d="M870,1167c-34-17-55-57-46-90c3-15,81-100,194-211l187-185l-565-1c-431,0-571-3-590-13c-55-28-64-94-18-137c21-20,33-20,597-20h575l-192-193C800,103,794,94,849,39c20-20,39-29,61-29c28,0,63,30,298,262c147,144,272,271,279,282c30,51,23,60-219,304C947,1180,926,1196,870,1167z">
+                                                </path>
+                                            </g>
+                                        </svg>
+
+                                    </button>
+                                    <img src="assets/img/introduction-visual.png" alt="Welcome">
+                                </div>
+                                <div class="intro--options">
+                                    <a href="#0">
+                                        <h3>Games</h3>
+                                        <p>Games 2D, 3D, games NFT, games em pixel art e muito mais.</p>
+                                    </a>
+                                    <a href="#0">
+                                        <h3>Bots</h3>
+                                        <p>Web Scraping, bots de automação, chat bots, bots NFT e muito mais.</p>
+                                    </a>
+                                    <a href="#0">
+                                        <h3>Apps</h3>
+                                        <p>WebApp, Android Apps, iOS Apps e muito mais.</p>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </li>
+
+                        <li class="l-section section">
+                            <div class="work section--prev">
+                                <h2>Produtos</h2>
+                                <div class="work--lockup">
+                                    <ul class="slider">
+
+                                        <li class="slider--item slider--item-center">
+                                            <a href="#0">
+                                                <div class="slider--item-image">
+                                                    <img src="assets/img/game-console.png" alt="Jogos">
+                                                </div>
+                                                <p class="slider--item-title">Desenvolvimento de games</p>
+                                                <p class="slider--item-description">- Unity</p>
+                                                <p class="slider--item-description">- Godot</p>
+
+
+
+                                            </a>
+                                        </li>
+                                        <li class="slider--item slider--item-left">
+                                            <a href="#0">
+                                                <div class="slider--item-image">
+                                                    <img src="assets/img/appdevelopment.png" alt="Apps">
+                                                </div>
+                                                <p class="slider--item-title">Desenvolvimento de Apps</p>
+                                                <p class="slider--item-description">- Flutter &nbsp;&nbsp;&nbsp;&nbsp;-
+                                                    Angular</p>
+                                                <p class="slider--item-description">- ReactJS &nbsp;&nbsp;&nbsp;&nbsp;-
+                                                    VueJS</p>
+
+
+
+                                            </a>
+                                        </li>
+                                        <li class="slider--item slider--item-right">
+                                            <a href="#0">
+                                                <div class="slider--item-image">
+                                                    <img src="assets/img/bot.png" alt="Bots">
+                                                </div>
+                                                <p class="slider--item-title">Desenvolvimento de Bots</p>
+                                                <p class="slider--item-description">- C#&nbsp;&nbsp; - NodeJS</p>
+                                                <p class="slider--item-description">- Python&nbsp;&nbsp; - Bots Discord
+                                                </p>
+                                            </a>
+                                        </li>
+
+                                        <li class="slider--item " <a="" href="#0">
+                                            <div class="slider--item-image">
+                                                <img src="assets/img/shopping-online.png" alt="ecommerce">
+                                            </div>
+                                            <p class="slider--item-title">Websites</p>
+                                            <p class="slider--item-description">- Landing Page</p>
+                                            <p class="slider--item-description">- WooCommerce</p>
+
+
+                                        </li>
+
+                                    </ul>
+                                    <div class="slider--prev">
+                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                            viewBox="0 0 150 118" style="enable-background:new 0 0 150 118;"
+                                            xml:space="preserve">
+                                            <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
+                                                <path d="M561,1169C525,1155,10,640,3,612c-3-13,1-36,8-52c8-15,134-145,281-289C527,41,562,10,590,10c22,0,41,9,61,29
+                    c55,55,49,64-163,278L296,510h575c564,0,576,0,597,20c46,43,37,109-18,137c-19,10-159,13-590,13l-565,1l182,180
+                    c101,99,187,188,193,199c16,30,12,57-12,84C631,1174,595,1183,561,1169z"></path>
+                                            </g>
+                                        </svg>
+                                    </div>
+                                    <div class="slider--next">
+                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                            viewBox="0 0 150 118" style="enable-background:new 0 0 150 118;"
+                                            xml:space="preserve">
+                                            <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
+                                                <path
+                                                    d="M870,1167c-34-17-55-57-46-90c3-15,81-100,194-211l187-185l-565-1c-431,0-571-3-590-13c-55-28-64-94-18-137c21-20,33-20,597-20h575l-192-193C800,103,794,94,849,39c20-20,39-29,61-29c28,0,63,30,298,262c147,144,272,271,279,282c30,51,23,60-219,304C947,1180,926,1196,870,1167z">
+                                                </path>
+                                            </g>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li class="l-section section">
+                            <div class="about">
+                                <div class="about--banner">
+                                    <h2>Start-up cooperativa focada em desenvolvimento de aplicativos, bots e games.
+                                    </h2>
+
+
+                                </div>--&gt;
+                            </div>
+                        </li>
+
+                        <li class="l-section section">
+                            <div class="contact">
+                                <div class="contact--lockup">
+                                    <div class="modal">
+                                        <div class="modal--information">
+                                            <p>Somos uma start-up totalmente digital e não temos endereço físico.</p>
+
+                                        </div>
+                                        <ul class="modal--options">
+
+
+                                            <p style="text-align: left;">Envie-nos uma mensagem falando um pouco sobre
+                                                seu projeto.
+
+                                            </p>
+                                            <p>
+
+
+
+
+                                            </p>
+                                            <form action="https://postmail.invotes.com/send" method="post"
+                                                id="email_form">
+
+                                                <label for="name">Nome:</label>
+                                                <input type="text" id="fname" name="subject"><br><br>
+                                                <label for="email">E-mail:</label>
+                                                <input type="text" id="email" name="text"><br><br>
+
+
+                                                <label for="project">Para qual produto você deseja
+                                                    informações?</label><br>
+                                                <br>
+                                                <label for="bots">Bots</label>
+                                                <input type="checkbox" id="bots" name="extra_bots" value="Bots"><br>
+                                                <label for="games">Games</label>
+                                                <input type="checkbox" id="games" name="extra_games" value="Games">
+                                                <br>
+                                                <label for="apps">Aplicativos (Android/iOS)</label>
+                                                <input type="checkbox" id="app" name="extra_app" value="App">
+                                                <br>
+                                                <label for="website">Website</label>
+                                                <input type="checkbox" id="website" name="extra_website"
+                                                    value="Websites">
+                                                <br>
+                                                <label for="e-commerce">E-commerce</label>
+                                                <input type="checkbox" id="e-commerce" name="extra_e-commerce"
+                                                    value="E-commerce">
+                                                <br><br>
+
+                                                <label for="message">Conte-nos um pouco sobre sua ideia no campo
+                                                    abaixo</label><br><br>
+                                                <textarea name="extra_msg" id="message"></textarea><br>
+
+                                                <input type="hidden" name="access_token"
+                                                    value="pmndx655nikasi9u2jn9qkkm">
+                                                <!-- return urls can be fully qualified -OR-
+                                                     start with / for root relative -OR-
+                                                     start with . for url relative -->
+                                                <input type="hidden" name="success_url"
+                                                    value=".?message=Email+Successfully+Sent%21&amp;isError=0">
+                                                <input type="hidden" name="error_url"
+                                                    value=".?message=Email+could+not+be+sent.&amp;isError=1">
+
+                                                <input id="submit_form" type="submit" value="Enviar">
+
+                                            </form>
+
+                                            <!-- optional, prevents the submit button from being pressed more than once -->
+                                            <script>
+                                                let submitButton = document.getElementById("submit_form");
+                                                let form = document.getElementById("email_form");
+                                                form.addEventListener("submit", function (e) {
+                                                    setTimeout(function () {
+                                                        submitButton.value = "Sending...";
+                                                        submitButton.disabled = true;
+                                                    }, 1);
+                                                });
+                                            </script>
+
+
+
+
+
+                                            <p></p>
+
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <!-- <canvas class="particles-js-canvas-el" width="2592" height="2940" style="width: 100%; height: 100%;"></canvas> -->
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="./assets/js/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+    <script src="./assets/js/script.js"></script>
 </body>
 
 </html>
